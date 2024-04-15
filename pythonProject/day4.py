32cd0c99058d81181f34083d0f3e1439f390fe8a
phuong2k4 <phuongsepfg@gmail.com>
2024-04-15 09:53:17 +0700

    Day4

--- pythonProject/day4.py
+++ pythonProject/day4.py
@@ -14,13 +14,29 @@
 # for ind in human:
 #     print(ind)
 
-names_string = "phuong,phong,quynh,bao"
-names = names_string.split(", ")
-import random
+# names_string = "phuong,phong,quynh,bao"
+# names = names_string.split(", ")
+# import random
+#
+# total = len(names)
+# random_in_string = random.randint(0, total - 1)
+# pay_guys = names[random_in_string]
+#
+# print(f"The pay guy today is {pay_guys}")
 
-total = len(names)
-random_in_string = random.randint(0, total - 1)
-pay_guys = names[random_in_string]
+list1 = [" ", " ", " "]
+list2 = [" ", " ", " "]
+list3 = [" ", " ", " "]
 
-print(f"The pay guy today is {pay_guys}")
+map = [list1, list2, list3]
 
+hidding_treasure = input("Where do you want to put a treasure?")
+
+letter = hidding_treasure[0].lower()
+text = ["a", "b", "c"]
+
+letter_index = text.index(letter)
+number_index = int(hidding_treasure[1])-1
+map[number_index][letter_index] = "X"
+
+print(f"{list1} \n {list2} \n {list3}")
