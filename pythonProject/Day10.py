b74b29a3b69fc11fab450bd709fa6afad3d60de8
phuong2k4 <phuongsepfg@gmail.com>
2024-04-21 20:02:26 +0700

    D10

--- pythonProject/Day10.py
+++ pythonProject/Day10.py
@@ -11,25 +11,52 @@
 # print(add_parameter(f_name, l_name))
 
 
-year = int(input("Year: "))
-month = int(input("Month: "))
+# year = int(input("Year: "))
+# month = int(input("Month: "))
+#
+# def is_leap(year):
+#     """
+#     Return leap year or not a leap year
+#     (You can just mod 4, result still corectly)
+#     """
+#     if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+#         return True
+#     else:
+#         return False
+#
+# def days_in_month(year,month):
+#     """This function will return a month in leap or not a leap year"""
+#     month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
+#     if month == 2 and is_leap(year):
+#         return 29
+#     else:
+#         return month_days[month - 1]
+#
+# print(days_in_month(year, month))
 
-def is_leap(year):
-    """
-    Return leap year or not a leap year
-    (You can just mod 4, result still corectly)
-    """
-    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
-        return True
-    else:
-        return False
 
-def days_in_month(year,month):
-    """This function will return a month in leap or not a leap year"""
-    month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
-    if month == 2 and is_leap(year):
-        return 29
-    else:
-        return month_days[month - 1]
+#solution Caculator:
 
-print(days_in_month(year, month))
+def add(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def muti(num1, num2):
+    return num1 * num2
+def dev(num1, num2):
+    return num1 / num2
+
+num1 = int(input("Whats the first number? "))
+operation = str(input("Pick an operation: "))
+num2 = int(input("How about the second number? "))
+
+if operation == "+":
+    print(f"{num1} {operation} {num2} = {add(num1,num2)}")
+elif operation == "-":
+    print(f"{num1} {operation} {num2} = {sub(num1,num2)}")
+elif operation == "*":
+    print(f"{num1} {operation} {num2} = {muti(num1, num2)}")
+elif operation == "/":
+    print(f"{num1} {operation} {num2} = {dev(num1, num2)}")
+
+
