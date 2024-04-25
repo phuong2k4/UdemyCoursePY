9968ba3c03e8ba6bf11927e3e11a4de3b1e2ab08
phuong2k4 <phuongsepfg@gmail.com>
2024-04-25 23:35:49 +0700

    D14 Testing 10:40 25/4

--- pythonProject/Day14.py
+++ pythonProject/Day14.py
@@ -4,19 +4,31 @@ from ExDay14 import Data
 print(art.logo)
 
 print("Who you think they have more followers?")
+def game_play():
+    compareA = random.choice(Data.data)
+    against = random.choice(Data.data)
+    end_game = False
+    point = 0
+    while not end_game:
+        print(f"{compareA["name"]} , {compareA["description"]} from {compareA["country"]}")
+        print(art.vs)
+        print(f"{against["name"]} , {against["description"]} from {against["country"]}")
+        user = input("Type A or B: ")
 
-compareA = random.choice(Data.data)
-against = random.choice(Data.data)
+        if user == "A" and compareA["follower_count"] > against["follower_count"]:
+            print("You're right. ")
+            against = random.choice(Data.data)
+            point+=1
+            print(f"Current score: {point}")
+        elif user == "B" and compareA["follower_count"] < against["follower_count"]:
+            print("You're right. ")
+            compareA = against
+            against = random.choice(Data.data)
+            point += 1
+            print(f"Current score: {point}")
+        else:
+            print("Lose")
+            end_game = True
+    print(f"your final point: {point}")
 
-print(compareA)
-print(art.vs)
-print(against)
-user = input("Type A or B: ")
-point = 0
-
-if user == "A" and compareA["follower_count"] > against["follower_count"]:
-    point += 1
-elif user == "B" and compareA["follower_count"] < against["follower_count"]:
-    point += 1
-else:
-    print("Lose")
+game_play()
