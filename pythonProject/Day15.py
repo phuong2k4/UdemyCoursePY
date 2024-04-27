b64762d40fe6c432ab586b528e1b6292fb0d50e8
phuong2k4 <phuongsepfg@gmail.com>
2024-04-27 09:59:50 +0700

    D15 make 10:00 27/4

--- pythonProject/Day15.py
+++ pythonProject/Day15.py
@@ -1,9 +1,14 @@
 #pycharm features
 # you can change all name function by retractor
 #using retractor -> rename
-def sum(n1, n2):
-    total = n1 + n2
-    return total
+# def sum(n1, n2):
+#     total = n1 + n2
+#     return total
+#
+#
+# print(sum(1, 4))
+#
+#solution coffee machine code
 
 
-print(sum(1, 4))
+
