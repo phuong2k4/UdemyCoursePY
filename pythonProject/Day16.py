81ff6af61843738361f585906f40acd8f614f409
phuong2k4 <phuongsepfg@gmail.com>
2024-04-30 21:15:08 +0700

    D16 debug 21:15 30/4

---
+++ pythonProject/Day16.py
@@ -0,0 +1,49 @@
+#objects and accessing
+
+#
+# from turtle import Turtle, Screen
+#
+# test = Turtle()
+# print(test)
+# test.shape("turtle")
+# test.color("coral")
+# test.backward(250)
+# my_Screen = Screen()
+#
+# print(my_Screen.canvheight)
+# my_Screen.exitonclick()
+
+# import turtle
+#
+# # Tạo một cửa sổ đồ họa
+# window = turtle.Screen()
+#
+# # Tạo một đối tượng rùa
+# alex = turtle.Turtle()
+#
+# # Vẽ hình vuông
+# for _ in range(4):
+#     alex.forward(100)  # Di chuyển về phía trước 100 bước
+#     alex.left(90)      # Rẽ trái 90 độ
+#
+# # Đóng cửa sổ khi nhấp chuột vào cửa sổ
+# window.mainloop()
+
+# graphics in py
+# from prettytable import PrettyTable
+#
+# windows = PrettyTable()
+# windows.field_names = ["Name", "Age" , "Gender"]
+# windows.add_row(["Phuong", 21, "Male"])
+# windows.add_row(["Nam", 21, "Male"])
+# windows.add_row(["Cuc", 22, "FeMale"])
+#
+# windows.align["Name"] = "l"   # Canh trái cho cột "Name"
+# windows.align["Age"] = "r"    # Canh phải cho cột "Age"
+# windows.align["Gender"] = "c"
+#
+# print(windows)
+
+
+# coffee machine use oop or object-oriented-programming
+
