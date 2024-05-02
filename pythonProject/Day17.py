79451119e72089e7a8d068e2a87d26ab2dede2f6
phuong2k4 <phuongsepfg@gmail.com>
2024-05-03 01:20:18 +0700

    D17 F 1h20 3/5

--- pythonProject/Day17.py
+++ pythonProject/Day17.py
@@ -1,9 +1,25 @@
 #class and create class in Python
 
-class Car:
-    def printOut(self):
-        print(self)
+# class Car:
+#     def __init__(self, id_car, name_car):
+#         self.id = id_car
+#         self.name = name_car
+#     def speed_car(self, kmh):
+#         print(kmh)
+#
+#
+# BMW_Car = Car(123, "BMW")
+#
+# print(BMW_Car.speed_car(190))
 
-Car.printOut(12)
+from ExDay17.question import question_data
+from ExDay17.question_model import Question
 
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
+print(question_bank[0].Ianswer)
 
