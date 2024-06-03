28ee4ceac836bb80149824a1a1087ac77c59a4ff
phuong2k4 <phuongsepfg@gmail.com>
2024-06-03 11:42:44 +0700

    Day 25 pandas file and us_state_game end 11h45 3/6

--- pythonProject/Day25.py
+++ pythonProject/Day25.py
@@ -1,7 +1,7 @@
 # with open("ExDay25/DataWeather.csv") as File:
 #     read = File.readlines()
 #     print(read)
-
+import turtle
 
 # import csv
 # with open("ExDay25/DataWeather.csv") as Data:
@@ -62,20 +62,55 @@ import pandas
 # data_n.to_csv("ExDay25/new_data.csv")
 
 # 004 challenge:
-data = pandas.read_csv("ExDay25/2018-data-nyc.csv")
+# data = pandas.read_csv("ExDay25/2018-data-nyc.csv")
 
 # export_file = pandas.DataFrame(furcolor_data)
-grey_count = len(data[data["Primary Fur Color"] == "Grey"])
-red_count = len(data[data["Primary Fur Color"] == "Red"])
-black_count = len(data[data["Primary Fur Color"] == "Black"])
+# grey_count = len(data[data["Primary Fur Color"] == "Grey"])
+# red_count = len(data[data["Primary Fur Color"] == "Red"])
+# black_count = len(data[data["Primary Fur Color"] == "Black"])
+#
+# fur_color = {
+#     "Fur color": ["Grey", "Red", "Black"],
+#     "Count": [
+#         grey_count,
+#         red_count,
+#         black_count
+#     ]
+# }
+# data_export = pandas.DataFrame(fur_color)
+# data_export.to_csv("ExDay25/ExportFile.csv")
 
-fur_color = {
-    "Fur color": ["Grey", "Red", "Black"],
-    "Count": [
-        grey_count,
-        red_count,
-        black_count
-    ]
-}
-data_export = pandas.DataFrame(fur_color)
-data_export.to_csv("ExDay25/ExportFile.csv")
+from turtle import *
+
+screen = Screen()
+screen.title("Us State Game")
+img = "ExDay25/blank_states_img.gif"
+
+screen.addshape(img)
+turtle.shape(img)
+
+def get_mouse_click_coor(x,y):
+    print(x, y)
+
+states_correct = []
+data_us_states = pandas.read_csv("ExDay25/50_states.csv")
+while len(states_correct) < 50:
+    answer_state = screen.textinput(title=f"{len(states_correct)}/50 Guess the States" , prompt="Whats the next state's name?")
+    if answer_state in data_us_states.state.to_list():
+        states_correct.append(answer_state)
+        tur = turtle.Turtle()
+        tur.hideturtle()
+        tur.penup()
+        states = data_us_states[data_us_states.state == answer_state]
+        tur.goto(int(states.x), int(states.y))
+        tur.write(answer_state)
+    if answer_state == "exit":
+        missing_state = []
+        for state in data_us_states.state.to_list():
+            if state not in states_correct:
+                missing_state.append(state)
+        export_missing = pandas.DataFrame(missing_state)
+        export_missing.to_csv("ExDay25/Missing_States.csv")
+        break
+screen.onscreenclick(get_mouse_click_coor)
+screen.mainloop()
