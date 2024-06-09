409ff0db6f36d6b4fed97900e324100a98b296d1
phuong2k4 <phuongsepfg@gmail.com>
2024-06-09 12:25:00 -0400

    Day27 tkinter module 23h25 9/6

--- pythonProject/Day27.py
+++ pythonProject/Day27.py
@@ -1,1 +1,16 @@
-# create init file
+# tkinter module
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI Tkinter Module")
+window.minsize(width= 500, height= 300)
+
+# Label
+my_label = tkinter.Label(text="I am a free lance", font=("JetBrains Mono", 24, "bold"))
+my_label.pack() # change label position
+
+
+
+
+
+window.mainloop()
